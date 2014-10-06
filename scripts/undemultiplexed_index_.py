6785dd285f1e63e41e616d541edc1ed384fa82d4
Maya Brandi <maya.brandi@scilifelab.se>
2014-10-06 10:14:49 +0200

    changing qc flagg

--- scripts/undemultiplexed_index_.py
+++ scripts/undemultiplexed_index_.py
@@ -78,16 +78,16 @@ class UndemuxInd():
         QC3 = nr_reads >= 100000
 
         if QC1 and QC2 and QC3:
-            return 'Pass'
+            return 'PASSED'
         else:
-            return 'Warning'
+            return 'FAILED'
 
     def set_result_file_udfs(self):
         """populates udfs: '% Perfect Index Reads' and 'Index QC'"""
         for samp_name, target_file in self.target_files.items():
             if samp_name in self.barcode_lane_statistics.keys():
                 s_inf = self.barcode_lane_statistics[samp_name]
-                target_file.udf['Index QC'] = self._index_QC(s_inf)
+                target_file.qc_flag = self._index_QC(s_inf)
                 set_field(target_file)
                 self.nr_samps_updat += 1
             else:
