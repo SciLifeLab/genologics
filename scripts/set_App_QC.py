cd8d4c8410560278c0cf9312b18302787ff14e12
Maya Brandi <maya.brandi@scilifelab.se>
2014-10-14 15:26:25 +0200

    new script to load appqc data

--- scripts/set_App_QC.py
+++ scripts/set_App_QC.py
@@ -62,13 +62,10 @@ class AppQC():
 
     def set_result_file_udfs(self):
         """populates the target file App QC udf"""
-        print self.target_files.items()
         for samp_name, target_file in self.target_files.items():
-            print >> sys.stderr,  samp_name
             if samp_name in self.app_QC.keys():
                 qc_passed = self.app_QC[samp_name]['automated_qc']['qc_passed']
                 sample = target_file.samples[0]
-                print >> sys.stderr,  sample.udf.items()
                 sample.udf['App QC'] = qc_passed
                 set_field(sample)
                 self.nr_samps_updat += 1
