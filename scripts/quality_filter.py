5c4cfc543101f4fdb9e2f4b209b394ee468d7606
Maya Brandi <maya.brandi@scilifelab.se>
2014-11-03 13:40:26 +0100

    adding fc to path

--- scripts/quality_filter.py
+++ scripts/quality_filter.py
@@ -64,7 +64,7 @@ class QualityFilter():
                 self._set_udfs(samp_name, target_file, lane)
             if self.nr_samps_updat:
                 self.abstract.append("LANE: {0} for {1} samples."
-                    "".format(lane, len(set(self.nr_samps_updat)))
+                    "".format(lane, str(len(set(self.nr_samps_updat))))
             if self.missing_samps:
                 self.abstract.append("The following samples are missing in Quality "
                     "Filter file: {0}.".format(', '.join(self.missing_samps)))
