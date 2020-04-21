85b7b09bed3c95a268541c14bbe9a0869a980d10
Edvard Englund <edvard.englund@medsci.uu.se>
2020-04-21 18:34:49 +0200

    Fix process instrument

--- genologics/entities.py
+++ genologics/entities.py
@@ -446,7 +446,6 @@ class Project(Entity):
     udt          = UdtDictionaryDescriptor()
     files        = EntityListDescriptor(nsmap('file:file'), File)
     externalids  = ExternalidListDescriptor()
-    instrument = EntityDescriptor('instrument', Instrument)
     # permissions XXX
 
 
@@ -609,7 +608,7 @@ class Process(Entity):
     udt               = UdtDictionaryDescriptor()
     files             = EntityListDescriptor(nsmap('file:file'), File)
     process_parameter = StringDescriptor('process-parameter')
-    intrument = StringDescriptor('instrument')
+    instrument = EntityDescriptor('instrument', Instrument)
 
     # process_parameters XXX
 
